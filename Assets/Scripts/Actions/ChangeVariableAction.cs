d8a1f9174d858b98314ea0214513a2cfbca0828f
Cecília Costa <cecier.costa@gmail.com>
2017-10-25 16:14:18 -0300

    Implementing change character on the factory

--- Assets/Scripts/Actions/ChangeVariableAction.cs
+++ Assets/Scripts/Actions/ChangeVariableAction.cs
@@ -21,7 +21,23 @@ public class ChangeVariableAction : ToolKitAction {
 		ToolKitEvent tkevent = new ToolKitEvent (copy);
 		trigger.TriggerEvent (tkevent);
 	}
-	public override bool isStarted(){
+
+
+    public override void Execute(GameObject colliding, GameObject gameobjct) {
+        if (persists)
+            GameVariablesManager.Instance.PersistsCondition(changeTo);
+        else {
+            GameVariablesManager.Instance.ChangeConditionValue(changeTo);
+        }
+
+        Condition copy = new Condition();
+        copy.Copy(changeTo);
+        ToolKitEventTrigger trigger = new ToolKitEventTrigger();
+        ToolKitEvent tkevent = new ToolKitEvent(copy);
+        trigger.TriggerEvent(tkevent);
+    }
+
+    public override bool isStarted(){
 		return true;
 	}
 	public override bool isFinished(){
