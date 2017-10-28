d35916683820efdb183c6b7107e3a979932b04ac
cecicosta <cecier.costa@gmail.com>
2017-10-27 22:32:29 -0300

    Showing enemies life; Implementing Corius murder consequence.

---
+++ Assets/Scripts/ScaleDinamically.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleDinamically : MonoBehaviour {
+    public bool invert = false;
+
+    public void ScaleNormalizedX(float amount) {
+        transform.localScale = new Vector3((invert ? -1 : 1) * amount, transform.localScale.y, transform.localScale.z);
+    }
+    public void ScaleNormalizedY(float amount) {
+        transform.localScale = new Vector3(transform.localScale.x, (invert ? -1 : 1) * amount, transform.localScale.z);
+    }
+    public void ScaleNormalizedZ(float amount) {
+        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, (invert ? -1 : 1) * amount);
+    }
+
+}
